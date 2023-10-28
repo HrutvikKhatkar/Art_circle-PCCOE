0aebb9ca308af908a8d6a4655ff976941d9de271
gautam-bhagat <gautambhagat.business@gmail.com>
2023-10-28 22:52:40 +0530

    Home Gallery Done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -8,6 +8,34 @@ function Home() {
       <div className="hero">
       PCCOE COLLEGE <br/>ART CIRCLE
       </div>
+
+
+      <h1 className="heading">GALLERY</h1>
+      <div className="gallery">
+        <img src={require("../../assest/gallery/386673130_213604664911579_5321186601242373671_n.webp")}/>
+        <img src={require("../../assest/gallery/386846514_1375938053320991_8294661223858316170_n.webp")}/>
+        <img src={require("../../assest/gallery/386910116_853497913080947_9078205582414283250_n.webp")}/>
+        <img src={require("../../assest/gallery/387266633_666368108924687_873904391766480933_n.webp")}/>
+        <img src={require("../../assest/gallery/387748795_291616593812460_4426006694341827453_n.webp")}/>
+        <img src={require("../../assest/gallery/387752731_188571004276812_8727380717794735222_n.webp")}/>
+        <img src={require("../../assest/gallery/387761972_1268540457185801_2407028983387968253_n.webp")}/>
+        <img src={require("../../assest/gallery/387764801_1249026682427594_7342529586774272107_n.webp")}/>
+        <img src={require("../../assest/gallery/387800962_322947380323717_7091452336123205494_n.webp")}/>
+        <img src={require("../../assest/gallery/391334654_1574743776628955_4717791155029649805_n.webp")}/>
+        <img src={require("../../assest/gallery/391341222_5837386142964463_7346135557720469690_n.webp")}/> 
+        
+        <img src={require("../../assest/gallery/386910116_853497913080947_9078205582414283250_n.webp")}/>
+        <img src={require("../../assest/gallery/387266633_666368108924687_873904391766480933_n.webp")}/>
+        <img src={require("../../assest/gallery/387748795_291616593812460_4426006694341827453_n.webp")}/>
+        <img src={require("../../assest/gallery/387752731_188571004276812_8727380717794735222_n.webp")}/>
+        <img src={require("../../assest/gallery/387761972_1268540457185801_2407028983387968253_n.webp")}/>
+        <img src={require("../../assest/gallery/387764801_1249026682427594_7342529586774272107_n.webp")}/>
+        <img src={require("../../assest/gallery/387800962_322947380323717_7091452336123205494_n.webp")}/>
+        <img src={require("../../assest/gallery/391334654_1574743776628955_4717791155029649805_n.webp")}/>
+        <img src={require("../../assest/gallery/391341222_5837386142964463_7346135557720469690_n.webp")}/>
+      </div>
+
+
     </div>
   )
 }
