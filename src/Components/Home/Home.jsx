1a6765251d221647ca4341c8f986adc1769ac107
gautam-bhagat <gautambhagat.business@gmail.com>
2023-10-28 17:03:30 +0530

    HOme UI basic Complete

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className="hero">
+      PCCOE COLLEGE <br/>ART CIRCLE
+      </div>
+    </div>
+  )
+}
+
+export default Home
